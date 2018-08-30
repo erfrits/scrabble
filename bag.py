dd870b26f5838dc5e46fa15bc2cb26674c2f54b3
erfrits <erika.frits@gmail.com>
2018-08-31 01:05:20 +0200

    Created v1 of Bag

--- bag.py
+++ bag.py
@@ -1,12 +1,30 @@
+import json
+import random as rnd
+from math import floor
+
+
 class Bag:
 
+    _letters = []
+
     def __init__(self):
-        letters = []
+        with open(".\\resource\\letterDist") as f:
+            lDict = json.load(f)
+        for l in lDict:
+            s = l * lDict[l]
+            self._letters += list(s)
+        for i in range(floor((rnd.random() + 1) * 25)):
+            rnd.shuffle(self._letters)
+        # print(self.letters)
 
     def draw(self, n):
-        return None
+        __lDrawn = []
+        for i in range(n):
+            rnd.shuffle(self._letters)
+            __lDrawn += self._letters.pop(floor(rnd.random() * len(self._letters)))
+        return __lDrawn
 
 
 if __name__ == '__main__':
     bag: Bag = Bag()
-    bag.draw(5)
+    print(bag.draw(5))
